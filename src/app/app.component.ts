3052e8e76053b1e29af98ef9caef898df466369e
steven <gonghaima@hotmail.com>
2017-10-26 00:56:02 +1100

    Firebase2 with notification api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "angularfire2/firestore";
+import { Observable } from "rxjs/Observable";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   items: Observable<any[]>;
   itemsList: any;
   constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+    this.items = db.collection("items").valueChanges();
     this.items.subscribe(response => {
       const data = response[0];
 
@@ -23,7 +23,30 @@ export class AppComponent {
       }
       const result = arr.join(',');
 
-      this.itemsList = result;
+      this.itemsList = result.split(',');
+      const options = {
+        body: this.itemsList,
+        icon: `https://www.straightsell.com.au/documents/attache.jpg`
+      };
+
+      //#region
+      // Let's check if the browser supports notifications
+      if (!('Notification' in window)) {
+        alert('This browser does not support desktop notification');
+      } else if (Notification['permission'] === 'granted') {
+        // Let's check whether notification permissions have already been granted
+        // If it's okay let's create a notification
+        const notification = new Notification('Price updated!', options);
+      } else if (Notification['permission'] !== 'denied') {
+        // Otherwise, we need to ask the user for permission
+        Notification.requestPermission(function(permission) {
+          // If the user accepts, let's create a notification
+          if (permission === 'granted') {
+            const notification = new Notification('Price updated!', options);
+          }
+        });
+      }
+      //#endregion
     });
   }
 }
